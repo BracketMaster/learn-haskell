2f4f91fac5f244ba11558861290fe271479bd3e4
Yehowshua Immanuel <programmed4jesus@gmail.com>
2021-12-01 23:41:41 -0500

    I'd say question 10 is done

--- ch3/exercises/question10.hs
+++ ch3/exercises/question10.hs
@@ -1,43 +1,68 @@
+-- $ghci
+-- ghci> :l question10.hs 
+-- [1 of 1] Compiling Question10       ( question10.hs, interpreted )
+-- Ok, one module loaded.
+-- ghci> passed
+-- True
 module Question10 where
 
-data Direction = Left | Right | Straight deriving (Show)
+data Direction = Left | Right | Straight deriving (Eq, Show)
 
-data Vec2   = Vec2 Integer Integer deriving (Show)
-data Ray2   = Ray2 Vec2 Vec2 deriving (Show)
+-- Note that Point2 and Vec2 while structurally identical
+-- are semantically different. Point2 is simple a point on
+-- the two dimensional cartesian plane. Vec2 is a vector in R2
+-- anchored at zero. Mathematically, taking the determinant
+-- of two points makes no sense, so here, we only take the
+-- determinant of two vectors.
+data Point2 = Point2 Integer Integer deriving (Show)
+data Vec2   = Vec2   Integer Integer deriving (Show)
 
-ray2_to_vec2 
-    (Ray2 (Vec2 i1 j1) (Vec2 i2 j2)) = 
-        (Vec2 (i2 - i1) (j2 - j1))
+-- The following function takes two points, p1 and p2, and
+-- returns a vector that when translated to p1 will point to
+-- p2.
+makeVec (Point2 i1 j1) (Point2 i2 j2) = Vec2 (i2 - i1) (j2 - j1)
 
--- A positive determinant means that first vector argument
+-- given a path composed of three points, p1, p2, and p3, we
+-- want to know if the path curves left, right, or goes straight
+path_direction p1 p2 p3 =
+    let vec1 = makeVec p1 p2 in
+    let vec2 = makeVec p2 p3 in
+    vec2 `direction_relative_to` vec1
+
+-- A positive determinant means that the first vector argument
 -- is to the right of the second vector argument.
--- A negative determinant means that first vector argument
+-- A negative determinant means that the first vector argument
 -- is to the left of the second vector argument.
+-- Note, it is assumed that the vectors are anchored at the origin.
 det2 (Vec2 i1 j1) (Vec2 i2 j2) = i1*j2 - i2*j1
 
-(direction_relative_to) this_vec ref_vec
+direction_relative_to this_vec ref_vec
     | dir < 0  = Question10.Left
     | dir > 0  = Question10.Right
     | dir == 0 = Question10.Straight
     where
         dir = det2 this_vec ref_vec
 
--- the following points correspond to the
--- image in resources/question10.svg
-a = Vec2 1 0
-b = Vec2 0 1
-c = Vec2 2 3
-d = Vec2 4 2
-e = Vec2 3 1
-f = Vec2 5 1
+path1a = Point2 1 0
+path1b = Point2 0 1
+path1c = Point2 1 4
 
-r0 = Ray2 a b
-r1 = Ray2 a c
-r2 = Ray2 a d
-r3 = Ray2 a e
-r4 = Ray2 a f
-r5 = Ray2 c d
-r6 = Ray2 c b
+path2a = Point2 2 0
+path2b = Point2 4 2
+path2c = Point2 3 3
 
-rays    = [r0, r1, r2, r3, r4, r5, r6]
-vectors = [ray2_to_vec2 ray | ray <- rays]
+path3a = Point2 3 4
+path3b = Point2 2 3
+path3c = Point2 2 1
+
+res1 = path_direction path1a path1b path1c
+res2 = path_direction path2a path2b path2c
+res3 = path_direction path3a path3b path3c
+
+test_vector = [
+    (res1, Question10.Right),
+    (res2, Question10.Left),
+    (res3, Question10.Left)]
+
+tester (result, direction) = result == direction
+passed = all tester test_vector
